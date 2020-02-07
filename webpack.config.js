28081ea825749baa4c3b707e46c4a3a498c58d3c
Paul Young <paul-young@live.co.uk>
2020-02-07 11:51:20 +0000

    add webpack conf

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DotenvWebpackPlugin = require('dotenv-webpack')
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'node',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader', 'ts-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new DotenvWebpackPlugin()
+  ]
+}
